5651d662230e0cb8249c12677a2ed743dcb522d4
rathinreddy1402 <rathinreddy1402@gmail.com>
2023-01-29 13:27:05 +0530

    1st commit

---
+++ JenkGit1/src/test/java/Demo/Sample1Test.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Sample1Test {
+
+	@Test
+	public void d1()
+	{
+		System.out.println("hi");
+	}
+}
---
+++ JenkGit1/src/test/java/Demo/Sample2Test.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Sample2Test {
+
+	@Test
+	public void d2()
+	{
+		System.out.println("hello");
+	}
+}
